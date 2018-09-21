95e94a3232974b85215a83a7eb63e6fa3e1e2f12
Aliaksader Zamirovski <studiaformanta@yandex.ru>
2018-09-21 16:26:19 +0300

    some styles added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
@@ -12,25 +14,28 @@ const Header = () => {
       </div>
       <nav className="header_nav">
         <ul>
-          <li><a href="#">GEOMAP</a></li>
-          <li><a href="#">HOME</a></li>
-          <li><a href="#">ABOUT</a></li>
-          <li><a href="#">FEATURES</a></li>
-          <li><a href="#">PRICING</a></li>
+          <li><a href="#" title="go to Geomap section">GEOMAP</a></li>
+          <li><a href="#" title="go to Home section">HOME</a></li>
+          <li><a href="#" title="go to About section">ABOUT</a></li>
+          <li><a href="#" title="go to Features section">FEATURES</a></li>
+          <li><a href="#" title="go to Pricing section">PRICING</a></li>
         </ul>
       </nav>
       <div className="header__search">
         <form role="search">
+          <button>
+            <FontAwesomeIcon icon={faSearch}/>
+          </button>
           <input 
             type="search" id="site-search" name="q"
             placeholder="Search geotrack"
             aria-label="Search through geotrack content" 
           />
-          <button>Search</button>
         </form>
       </div>
       <div className="header__avatar">
         <img src="/images/avatar.png" alt="Avatar" />
+        <div aria-hidden></div>
       </div>
     </header>
   );
